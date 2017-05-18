81402ceb9d52e6f81ad3ccd2a1551609c638a650
BrieucKyo <brieucmailpro@gmail.com>
2017-05-18 23:32:06 +0200

    Modif hover button homepage & modif structure menu.html

--- js/scriptmain.js
+++ js/scriptmain.js
@@ -61,47 +61,47 @@ $(document).ready(function() {
 
     /* SWIPER */
     var galleryTop = new Swiper('.gallery-top', {
-   nextButton: '.swiper-button-next',
-   prevButton: '.swiper-button-prev',
-   spaceBetween: 10,
-   keyboardControl: true
-});
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-   spaceBetween: 10,
-   centeredSlides: true,
-   slidesPerView: 'auto',
-   touchRatio: 0.2,
-   slideToClickedSlide: true
-});
-galleryTop.params.control = galleryThumbs;
-galleryThumbs.params.control = galleryTop;
+       nextButton: '.swiper-button-next',
+       prevButton: '.swiper-button-prev',
+       spaceBetween: 10,
+       keyboardControl: true
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+       spaceBetween: 10,
+       centeredSlides: true,
+       slidesPerView: 'auto',
+       touchRatio: 0.2,
+       slideToClickedSlide: true
+    });
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
 
-var sliders = document.querySelectorAll('.swiper-slide');
-for (var i = 0; i < sliders.length; ++i) {
-   sliders[i].addEventListener('click', function(event) {
-      event.target.parentNode.parentNode.parentNode.classList.add('fullscreen');
-      setTimeout(function() {
-         galleryTop.update();
-         galleryThumbs.update();
-      }, 200);
-   }, false);
-}
+    var sliders = document.querySelectorAll('.swiper-slide');
+    for (var i = 0; i < sliders.length; ++i) {
+       sliders[i].addEventListener('click', function(event) {
+          event.target.parentNode.parentNode.parentNode.classList.add('fullscreen');
+          setTimeout(function() {
+             galleryTop.update();
+             galleryThumbs.update();
+          }, 200);
+       }, false);
+    }
 
-var closeButtons = document.querySelectorAll('.close-button');
-for (var y = 0; y < closeButtons.length; ++y) {
-   closeButtons[y].addEventListener('click', function(event) {
-      console.log(event);
-      var fullScreenElements = document.querySelectorAll('.fullscreen');
-      console.log(fullScreenElements);
-      for (var x = 0; x < fullScreenElements.length; ++x) {
-         fullScreenElements[x].classList.remove('fullscreen');
-         setTimeout(function() {
-            galleryTop.update();
-            galleryThumbs.update();
-         }, 200);
-      }
-   });
-}
+    var closeButtons = document.querySelectorAll('.close-button');
+    for (var y = 0; y < closeButtons.length; ++y) {
+       closeButtons[y].addEventListener('click', function(event) {
+          console.log(event);
+          var fullScreenElements = document.querySelectorAll('.fullscreen');
+          console.log(fullScreenElements);
+          for (var x = 0; x < fullScreenElements.length; ++x) {
+             fullScreenElements[x].classList.remove('fullscreen');
+             setTimeout(function() {
+                galleryTop.update();
+                galleryThumbs.update();
+             }, 200);
+          }
+       });
+    }
 
     /* HOVER MENU */
 	$(".hovermenu1").hover(function(){
@@ -143,7 +143,5 @@ for (var y = 0; y < closeButtons.length; ++y) {
     		$('.hovermenu4').addClass('menuhover');
     	}
 	});
-
-
 
 });
