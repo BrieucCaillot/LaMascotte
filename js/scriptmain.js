3bc5269f45137432e12370e537c2584b8485ad52
BrieucKyo <brieucmailpro@gmail.com>
2017-05-23 00:22:38 +0200

    Modif progress bar on scroll + modif swiper index.html + modif footer padding

--- js/scriptmain.js
+++ js/scriptmain.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    /* FIXED MENU */
+    // FIXED MENU 
     $(document).on("scroll", function() {
 
         if ($(document).scrollTop()>80) {
@@ -9,7 +9,6 @@ $(document).ready(function() {
                 "z-index": "200",
                 "min-height": "60px",
                 "background-color": "rgb(107, 151, 127)",
-                "border-bottom": "3px solid #e0d2b1"
             });
 
             $("header nav").css({
@@ -58,6 +57,18 @@ $(document).ready(function() {
             });
         }
     }); 
+
+    // PROGRESS BAR ON SCROLL
+    function updateProgress(num1, num2){
+        var percent = Math.ceil( num1 / num2 * 100 ) + '%';
+        document.getElementById('progress').style.width = percent;
+    }
+
+    window.addEventListener('scroll', function(){
+        var top = window.scrollY;
+        var height = document.body.getBoundingClientRect().height - window.innerHeight;
+        updateProgress(top, height);
+    });
 
     /* SWIPER */
     var galleryTop = new Swiper('.gallery-top', {
