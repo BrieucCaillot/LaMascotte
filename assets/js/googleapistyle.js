a5c91d2ddca4929f59a230c3927891bb6a5fd51c
BrieucKyo <brieucmailpro@gmail.com>
2017-06-13 00:20:17 +0200

    Modif folders and roots of project for node

---
+++ assets/js/googleapistyle.js
@@ -0,0 +1,111 @@
+var map;
+var marker;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 48.885720, lng: 2.335444},
+        zoom: 15 ,
+        styles : style,
+        scrollwheel: false,
+        streetViewControl: false,
+        mapTypeControl: false
+    });
+    marker = new google.maps.Marker({
+        map: map,
+        icon: 'assets/images/main/mascottelogomapp.png',
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 48.885720, lng: 2.335444}
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+} 
+
+var style = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#145772"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+
+
